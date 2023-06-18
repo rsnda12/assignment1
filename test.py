5bfd2bf442eb8933cfad9568f706ed6393ee7118
rsnda12 <sundararaman0@gmail.com>
2023-06-18 21:44:22 +0530

    msg

---
+++ test.py
@@ -0,0 +1,14 @@
+print('hi how are you?')
+
+print('i am fine')
+
+class person():
+    def __init__(self,name,surname,emailid,date_of_birth):
+        self.name=name
+        self.surname=surname
+        self.emailid=emailid
+        self.date_of_birth=date_of_birth
+sundar=person('sundar','Radhakrishnan','sundararaman0@gmail.com',12-9-1994) 
+print(sundar.name)
+print(sundar.date_of_birth)
+print(sundar.emailid)
